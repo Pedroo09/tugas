d461e461442aa244dc57a32e8a914b7df6cf7c86
pedroinaibaho09@gmail.com <pnaibaho64@gmail.com>
2023-03-27 22:35:25 +0700

    first commit

---
+++ aplikasi.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OPERASI ARITMATIKA</title>
+</head>
+<body>
+    <h5> APLIKASI PERHITUNGAN MATEMATIKA </h5>
+    <form action="proses.php" method="POST">
+        Bilangan 1 : <br>
+        <input type="text" name="txtbil1"><br>
+        Bilangan 2 : <br>
+        <input type="text" name="txtbil2"><br>
+        <button type="submit">HASIL</button>
+    </form>
+</body>
+</html>
